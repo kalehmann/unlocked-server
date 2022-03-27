a2cd4403c797707808cab0cc3fb4bc3989c522d0
Karsten Lehmann <mail@kalehmann.de>
2022-03-27 11:50:52 +0200

    feat: Key model

---
+++ src/Model/Key.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ *  Copyright (C) 2022 Karsten Lehmann <mail@kalehmann.de>
+ *
+ *  This file is part of unlocked-server.
+ *
+ *  unlocked-server is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  unlocked-server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with unlocked-server. If not, see
+ *  <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace KaLehmann\UnlockedServer\Model;
+
+class Key
+{
+    private bool $deleted = false;
+
+    private string $description;
+
+    private string $handle;
+
+    private string $key;
+
+    private User $user;
+
+    public function __construct(
+        string $handle,
+        string $key,
+        User $user,
+    ) {
+        $this->handle = $handle;
+        $this->key = $key;
+        $this->user = $user;
+    }
+
+    public function isDeleted(): bool
+    {
+        return $this->deleted;
+    }
+
+    public function delete(): void
+    {
+        $this->deleted = true;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getHandle(): string
+    {
+        return $this->handle;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+}
