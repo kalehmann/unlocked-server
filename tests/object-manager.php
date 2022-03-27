389ab862787061b3ff977b7e61b5e06d88984d58
Karsten Lehmann <mail@kalehmann.de>
2022-03-27 16:32:18 +0200

    feat: Installed and configured phpstan-doctrine

---
+++ tests/object-manager.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *  Copyright (C) 2022 Karsten Lehmann <mail@kalehmann.de>
+ *
+ *  This file is part of unlocked-server.
+ *
+ *  unlocked-server is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  unlocked-server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with unlocked-server. If not, see
+ *  <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+use KaLehmann\UnlockedServer\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
