06842242d99a5e6747d962df8f76a3333ec40dd8
Karsten Lehmann <mail@kalehmann.de>
2022-05-05 20:20:19 +0200

    fix: Remove unused import

--- src/Form/Type/DenyRequestType.php
+++ src/Form/Type/DenyRequestType.php
@@ -23,12 +23,10 @@ declare(strict_types=1);
 
 namespace KaLehmann\UnlockedServer\Form\Type;
 
-use KaLehmann\UnlockedServer\Model\Request;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DenyRequestType extends AbstractType
